bc0412236bd626b7da9a2a8a869ca50bcdbb43b9
Milan199608 <executive100007@gmail.com>
2021-10-05 16:51:49 +0530

    navbarcss some changes

--- Client/src/Component/Navbar/NavBar.js
+++ Client/src/Component/Navbar/NavBar.js
@@ -1,52 +1,46 @@
 
-import React,{useState} from 'react';
+import React from 'react';
 import './NavBar.css';
-import SlidingPane from "react-sliding-pane";
+
 
 const NavBar = () => {
 
- 
+
   return (
     <div>
-     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-toggler-icon" href="#">
-  <div style={{ marginTop: "32px" }}>
-        <button >
-         
-        </button>
-        
-      </div>
-  </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+      <nav class="navbar navbar-expand-lg  fixed-top navbar-dark bg-dark ">
+        <div className="navBar_toggle_style">
+        <a class="navbar-toggler-icon mx-2" href="#">
+        </a>
+    
+        <p style={{color:"white"}} >Zirtual</p>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Who We Are </a>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav ms-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Who We Are </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Who We Serve</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">What We Do</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">FAQs</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pricing</a>
+            </li>
 
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Who We Serve</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">What We Do</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">FAQs</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-   
-    </ul>
+          </ul>
 
- 
-      <button class="btn btn-success my-2 my-sm-0" type="submit">Request A Consultation</button>
-  
-  </div>
-</nav>
+
+          <button class="btn btn-success my-2 my-sm-0" type="submit">Request A Consultation</button>
+
+        </div>
+      </nav>
 
     </div>
   )
